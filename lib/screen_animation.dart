d45f6d886d8bd2ffbc23a99f403d7b1b740e2db8
EmpiereTech-Avadh <developer.empiere@gmail.com>
2025-03-26 15:52:01 +0530

    - Screen Animation: Created the animation class to render the animation screen UI. - main: Removed the counter app working and added the animation screen. - AndroidManifest: Refactored the App name.

---
+++ lib/screen_animation.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class ScreenAnimation extends StatefulWidget {
+  const ScreenAnimation({super.key});
+
+  @override
+  State<ScreenAnimation> createState() => _ScreenAnimationState();
+}
+
+class _ScreenAnimationState extends State<ScreenAnimation> with SingleTickerProviderStateMixin {
+  late AnimationController ctrAnimation;
+  late Animation<double> widthAnimation;
+  late Animation<Color?> colorAnimation;
+  late Animation<double> borderRadiusAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    ctrAnimation = AnimationController(vsync: this, duration: const Duration(seconds: 2));
+
+    widthAnimation = Tween<double>(
+      begin: 0,
+      end: 200,
+    ).animate(CurvedAnimation(parent: ctrAnimation, curve: Curves.easeInOut));
+
+    colorAnimation = ColorTween(
+      begin: Colors.purple.shade400,
+      end: Colors.pink.shade400,
+    ).animate(CurvedAnimation(parent: ctrAnimation, curve: Curves.easeInCubic));
+
+    borderRadiusAnimation = Tween<double>(
+      begin: 0.0,
+      end: 50.0,
+    ).animate(CurvedAnimation(parent: ctrAnimation, curve: Curves.easeInOut));
+
+    ctrAnimation.forward();
+  }
+
+  @override
+  void dispose() {
+    ctrAnimation.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: AnimatedBuilder(
+          animation: ctrAnimation,
+          builder: (context, child) {
+            return Center(
+              child: Container(
+                width: widthAnimation.value,
+                height: widthAnimation.value,
+                decoration: BoxDecoration(
+                  color: colorAnimation.value,
+                  borderRadius: BorderRadius.circular(borderRadiusAnimation.value),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
